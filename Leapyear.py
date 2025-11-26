525aae6db395aeccc1ef04b1d6c8afce3acfce5f
MEGHANA PRUTHVI <126481796+MEGHAPRU03@users.noreply.github.com>
2025-11-26 22:36:18 +0530

    Leap year lab program

---
+++ Leapyear.py
@@ -0,0 +1,8 @@
+print('Check whether its a leap year or not?')
+
+a=int(input("Enter a number:"))
+
+if (a%4==0 and a%100!=0) or (a%400==0):
+    print("Leap year")
+else:
+    print("Not a leap year")
